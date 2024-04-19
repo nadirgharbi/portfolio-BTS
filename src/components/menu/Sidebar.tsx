0234aa99de16e20d6ee0cccfffdd0430fc30a5b9
nadirgharbi <116977046+nadirgharbi@users.noreply.github.com>
2024-04-19 10:39:33 +0200

    sidebar modified for displayed w/ map

--- src/components/menu/Sidebar.tsx
+++ src/components/menu/Sidebar.tsx
@@ -3,38 +3,35 @@ import { BsHouse, BsHouseFill, BsPersonVcard, BsPersonVcardFill, BsPersonGear, B
 import { NavLink, useLocation } from "react-router-dom";
 import { Separator } from "../ui/separator";
 import { ModeToggle } from "../theme/ModeToggle";
+import { IconType } from "react-icons/lib";
 
 export const Sidebar = () => {
   const location = useLocation();
   const [active, setActive] = useState<string>(location.pathname);
-  const activeCss = "bg-secondary-color hover:bg-secondary-color/95 text-default-color rotate-12";
+  const activeCss = "bg-secondary-color hover:bg-secondary-color/95 text-default-color rotate-[15deg]";
   const globalCss = "p-2 rounded-full transition-transform";
+
+  const sidebarItems = [
+    { path: "/", label: "Accueil", icon: <BsHouse size={32} />, iconFill: <BsHouseFill size={32} /> },
+    { path: "/about", label: "À Propos", icon: <BsPersonVcard size={32} />, iconFill: <BsPersonVcardFill size={32} /> },
+    { path: "/skills", label: "Compétences", icon: <BsPersonGear size={32} />, iconFill: <BsPersonFillGear size={32} /> },
+    { path: "/projects", label: "Projets", icon: <BsFileEarmarkCode size={32} />, iconFill: <BsFileEarmarkCodeFill size={32} /> },
+    { path: "/monitoring", label: "Veilles", icon: <BsBinoculars size={32} />, iconFill: <BsBinocularsFill size={32} /> },
+    { path: "/contact", label: "Contact", icon: <BsEnvelope size={32} />, iconFill: <BsEnvelopeFill size={32} /> },
+  ];
 
   useEffect(() => {
     setActive(location.pathname);
   }, [location.pathname]);
 
   return (
-    <div className="w-14 absolute h-screen flex items-center">
+    <div className="w-14 absolute h-screen flex items-center ">
       <div className="flex flex-col items-center justify-center gap-2 py-3 px-8 fixed z-[999] left-4 rounded-2xl w-14 bg-default-color dark:bg-primary-color text-primary-color dark:text-default-color border border-primary-color/20 dark:border-default-color/20">
-        <NavLink to={"/"} className={`${globalCss} ${active === "/" ? activeCss : ""}`}>
-          {active === "/" ? <BsHouseFill size={32} /> : <BsHouse size={32} />} {/* On vérifie si le chemin courrant correspond au composant concernés */}
-        </NavLink>
-        <NavLink to={"/about"} className={`${globalCss} ${active === "/about" ? activeCss : ""}`}>
-          {active === "/about" ? <BsPersonVcardFill size={32} /> : <BsPersonVcard size={32} />}
-        </NavLink>
-        <NavLink to={"/skills"} className={`${globalCss} ${active === "/skills" ? activeCss : ""}`}>
-          {active === "/skills" ? <BsPersonFillGear size={32} /> : <BsPersonGear size={32} />}
-        </NavLink>
-        <NavLink to={"/projects"} className={`${globalCss} ${active === "/projects" ? activeCss : ""}`}>
-          {active === "/projects" ? <BsFileEarmarkCodeFill size={32} /> : <BsFileEarmarkCode size={32} />}
-        </NavLink>
-        <NavLink to={"/monitoring"} className={`${globalCss} ${active === "/monitoring" ? activeCss : ""}`}>
-          {active === "/monitoring" ? <BsBinocularsFill size={32} /> : <BsBinoculars size={32} />}
-        </NavLink>
-        <NavLink to={"/contact"} className={`${globalCss} ${active === "/contact" ? activeCss : ""}`}>
-          {active === "/contact" ? <BsEnvelopeFill size={32} /> : <BsEnvelope size={32} />}
-        </NavLink>
+        {sidebarItems.map((item) => (
+          <NavLink to={item.path} className={`${globalCss} ${active === item.path ? activeCss : ""}`}>
+            {active === item.path ? item.iconFill : item.icon} {/* On vérifie si le chemin courrant correspond au composant concernés */}
+          </NavLink>
+        ))}
         <Separator />
         <ModeToggle />
       </div>
