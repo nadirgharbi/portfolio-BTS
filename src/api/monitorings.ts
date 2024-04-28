14532f3e762cb14b859c32ea989626c5308d7b5e
Nadir GHARBI <nadirgharbi10@gmail.com>
2024-04-28 23:51:35 +0200

    change request

--- src/api/monitorings.ts
+++ src/api/monitorings.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const getAll = async () => {
   try {
-    const response = await axios.get("http://localhost:3333/api/veilles");
+    const response = await axios.get("https://portfolio-api-xi3a.onrender.com/api/veilles");
     return response.data;
   } catch (error) {
     return error;
