fc4d6a3f81b96ee25a5aa3442a135dcb93ebc82a
Nadir GHARBI <nadirgharbi10@gmail.com>
2025-05-25 21:32:12 +0200

    portfolio v2.1 - ok to deploy

--- src/components/etc/ContactInfos.tsx
+++ src/components/etc/ContactInfos.tsx
@@ -3,7 +3,7 @@ import { ContactInfo } from "./ContactInfo";
 export const ContactInfos: React.FC<ContactInfosProps> = ({ infos }) => {
 	return (
 		<>
-			<div className={`grid md:grid-cols-${infos.length} gap-0`}>
+			<div className={`grid md:grid-cols-3 gap-0`}>
 				{infos.map((info, key) => (
 					<ContactInfo {...info} key={key} />
 				))}
